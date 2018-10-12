9bc7a7b41ad26c621ed60e07a2aa2d8abddffc3e
upadhyay1808 <43991066+upadhyay1808@users.noreply.github.com>
2018-10-12 20:47:37 +0530

    i have shortened the string and use a for loop to print strings.

--- strings.cc
+++ strings.cc
@@ -1,13 +1,12 @@
-#include <cstdio>
 #include <iostream>
 #include <string>
-
 using namespace std;
-
 int main()
 {
-    string s = "Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!Hello World!";
-    cout << "sizeof s: " << sizeof(s) << endl;
-    cout << "sizeof string: " << sizeof(string) << endl;
+
+    string s="hello world!";
+	for(int i=0;i<s.length();i++)
+	cout<<s.at(i);
+	return(0);
 }
 
