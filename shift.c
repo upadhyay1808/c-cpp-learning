3d638503155686354c36a7bf7d801aefdd300834
upadhyay1808 <43991066+upadhyay1808@users.noreply.github.com>
2018-10-13 11:15:23 +0530

    Update shift.c

--- shift.c
+++ shift.c
@@ -1,16 +1,28 @@
 #include <stdio.h>
-
-int main(void)
+int main()
 {
-    char c = 123;
-    c <<= 8;
-    printf("%d\n", c); //Should print 0
-    c = 123;
-    c <<= 16;
-    printf("%d\n", c); //Should also print 0
-    int i = 234;
-    i <<= 32;
-    printf("%d\n", i); //Should print 234
+    int x,n,s;
+    printf("choose the shift operation you want to perform:");
+    scanf("%d",&n);
+    printf("enter the number:");
+    scanf("%d",&x);
+    printf("enter the no of bits you want to shift");
+    scanf("%d",&s);
+    switch(n)
+    {
+		case 1:
+    	{
+			printf("left shift operarion will give:");
+			printf("%d\n",x<<s);
+			break;
+		}
+		case 2:
+		{
+			printf("right shift operarion will give:");
+			printf("%d\n",x>>s);
+			break;
+		}
+	}
     return 0;
 }
 
